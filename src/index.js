7176fdd46cb9534ba8a23a05009c1e84b68b0de4
Hari Shanker Yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-03-03 00:14:50 +0530

    basic setup completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+const { PORT } = require('./config/serverConfig');
 const prepareAndStartServer = () => {
 
-    app.listen(3001, () => {
-        console.log(`server started at`);
+    app.listen(PORT, () => {
+        console.log(`server started at: ${PORT}`);
     });
 }
 
